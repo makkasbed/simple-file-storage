2bfead4a0ea0207b089c6eeff6ca2ceef3f86b38
adu <aluta182004@gmail.com>
2022-11-29 19:01:30 +0000

    api starting

--- api/controllers/accounts.go
+++ api/controllers/accounts.go
@@ -1,12 +1,60 @@
 package controllers
 
-import "logiclabent.com/sfs-api/models"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"logiclabent.com/sfs-api/models"
+	"logiclabent.com/sfs-api/utils"
+)
+
+var user string = utils.EnvVariable("DB_USER")
+var passwd = utils.EnvVariable("DB_PASSWD")
+var dbname = utils.EnvVariable("DB_NAME")
+var dbhost = utils.EnvVariable("DB_HOST")
+var jwtKey = utils.EnvVariable("SECRET")
 
 func CreateAccount(t models.Account) {
+	db, err := sql.Open("mysql", user+":"+passwd+"@tcp("+dbhost+")/"+dbname)
 
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	//insert, err := db.Exec()
 }
 func ListAccounts(t models.Account) []models.Account {
-	return nil
+	db, err := sql.Open("mysql", user+":"+passwd+"@tcp("+dbhost+")/"+dbname)
+
+	if err != nil {
+		fmt.Println("Err", err.Error())
+		return nil
+	}
+
+	defer db.Close()
+
+	results, err := db.Query("SELECT * FROM tbaccounts")
+	if err != nil {
+		fmt.Println("Err", err.Error())
+		return nil
+	}
+	accounts := []models.Account{}
+
+	for results.Next() {
+		var account models.Account
+
+		err = results.Scan(&account.Id)
+		if err != nil {
+			panic(err.Error())
+
+		}
+		accounts = append(accounts, account)
+	}
+
+	return accounts
 }
 func ListAccount(id string) models.Account {
 	var account models.Account
--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,8 @@
 module logiclabent.com/sfs-api
 
 go 1.18
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/joho/godotenv v1.4.0
+)
