1552730dc5b4064cdbdc74366085fefbfe0e4a94
adu <aluta182004@gmail.com>
2022-11-30 19:21:21 +0000

    implemented regions

--- api/controllers/regions.go
+++ api/controllers/regions.go
@@ -1,9 +1,31 @@
 package controllers
 
-import "logiclabent.com/sfs-api/models"
+import (
+	"database/sql"
+	"fmt"
 
-func CreateRegion(r models.Region) {
+	"logiclabent.com/sfs-api/models"
+)
 
+func CreateRegion(r models.Region) int {
+	db, err := sql.Open("mysql", user+":"+passwd+"@tcp("+dbhost+")/"+dbname)
+
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	insert, err := db.Exec("insert into tbregions(name,short_name,created_at,status)values(?,?,NOW(),?)", r.Name, r.ShortName, r.Status)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	rows, err := insert.LastInsertId()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return int(rows)
 }
 func ListRegions() []models.Region {
 	return nil
