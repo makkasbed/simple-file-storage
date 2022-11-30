52cae738527eadd01d2bff4d9dd2ef4ba4d1a3c7
adu <aluta182004@gmail.com>
2022-11-30 18:52:56 +0000

    implemented buckets

--- api/controllers/buckets.go
+++ api/controllers/buckets.go
@@ -1,22 +1,107 @@
 package controllers
 
-import "logiclabent.com/sfs-api/models"
+import (
+	"database/sql"
+	"fmt"
+
+	"logiclabent.com/sfs-api/models"
+)
 
 func CreateBucket(b models.Bucket) {
+	db, err := sql.Open("mysql", user+":"+passwd+"@tcp("+dbhost+")/"+dbname)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query("insert into tbbucket(name,description,created_at,status,url,account_id,bucket_type,versioning)values(?,?,NOW(),?,?,?,?)", b.Name, b.Description, b.Status, b.Url, b.CreatedBy, b.BucketType, b.Versioning)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
 
 }
 func ListBuckets(accountId string) []models.Bucket {
-	return nil
-}
-func ListBucket(bucketId string) models.Bucket {
-	var bucket models.Bucket
+	db, err := sql.Open("mysql", user+":"+passwd+"@tcp("+dbhost+")/"+dbname)
 
+	if err != nil {
+		fmt.Println("Err", err.Error())
+		return nil
+	}
+
+	defer db.Close()
+
+	results, err := db.Query("SELECT * FROM tbbuckets")
+	if err != nil {
+		fmt.Println("Err", err.Error())
+		return nil
+	}
+	buckets := []models.Bucket{}
+
+	for results.Next() {
+		var bucket models.Bucket
+
+		err = results.Scan(&bucket.Id, &bucket.Name, &bucket.Description, &bucket.BucketType, &bucket.CreatedAt, &bucket.CreatedBy, &bucket.Versioning)
+		if err != nil {
+			panic(err.Error())
+
+		}
+		buckets = append(buckets, bucket)
+	}
+
+	return buckets
+}
+func ListBucket(bucketId string) *models.Bucket {
+	bucket := &models.Bucket{}
+
+	db, err := sql.Open("mysql", user+":"+passwd+"@tcp("+dbhost+")/"+dbname)
+
+	if err != nil {
+		fmt.Println("Err", err.Error())
+		return nil
+	}
+
+	defer db.Close()
+
+	results, err := db.Query("SELECT * FROM tbbuckets where id=?", bucketId)
+	if err != nil {
+		fmt.Println("Err", err.Error())
+	}
+
+	if results.Next() {
+
+		err = results.Scan(&bucket.Id, &bucket.Name, &bucket.Description, &bucket.CreatedAt, &bucket.CreatedBy, &bucket.Region, &bucket.Url, &bucket.Status)
+		if err != nil {
+			fmt.Println(err.Error())
+
+		}
+
+	}
 	return bucket
 }
 func DeleteBucket(bucketId string) {
+	db, err := sql.Open("mysql", user+":"+passwd+"@tcp("+dbhost+")/"+dbname)
 
-}
+	if err != nil {
+		fmt.Println("Err", err.Error())
+	}
+	defer db.Close()
 
-func UpdateBucket(b models.Bucket) {
+	deleted, err := db.Exec("delete from tbbuckets where id=?", bucketId)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	num, err := deleted.RowsAffected()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if num > 0 {
+
+	}
 
 }
