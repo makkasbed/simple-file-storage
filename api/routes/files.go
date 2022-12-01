ab1c4b6c60e08d614e0ae98b9ba38a3505d5a833
adu <aluta182004@gmail.com>
2022-12-01 18:15:19 +0000

    files implemented

--- api/routes/files.go
+++ api/routes/files.go
@@ -1,1 +1,43 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"logiclabent.com/sfs-api/controllers"
+	"logiclabent.com/sfs-api/models"
+)
+
+func CreateFile(c *gin.Context) {
+	var file models.File
+
+	if err := c.BindJSON(&file); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+	} else {
+		controllers.CreateFile(file)
+		c.IndentedJSON(http.StatusCreated, file)
+	}
+}
+
+func GetFile(c *gin.Context) {
+	id := c.Param("key")
+	file := controllers.GetFile(id)
+
+	if file == nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.IndentedJSON(http.StatusOK, file)
+	}
+}
+
+func DeleteFile(c *gin.Context) {
+	id := c.Param("key")
+	num := controllers.DeleteFile(id)
+
+	if num <= 0 {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+
+		c.IndentedJSON(http.StatusOK, "success")
+	}
+}
